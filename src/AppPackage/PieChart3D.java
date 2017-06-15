d6f07301a199ecf853713f8e0ca0af631c891a51
sprintell <abayomi@sprintell.org>
2017-06-15 19:32:59 +0100

    Removing redundancies

--- src/AppPackage/PieChart3D.java
+++ src/AppPackage/PieChart3D.java
@@ -44,14 +44,10 @@ package AppPackage;
 
 import java.awt.Color;
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PiePlot3D;
 import org.jfree.data.general.DefaultPieDataset;
 import org.jfree.data.general.PieDataset;
-import org.jfree.ui.ApplicationFrame;
-import org.jfree.ui.RefineryUtilities;
 import org.jfree.util.Rotation;
 
 /**
